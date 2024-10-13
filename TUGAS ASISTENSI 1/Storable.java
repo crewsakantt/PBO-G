34c53f5fe697128480c3c33eaae153346f917d28
ammargif <161625541+ammargif@users.noreply.github.com>
2024-10-13 23:27:22 +0700

    Update Storable.java

--- TUGAS ASISTENSI 1/Storable.java
+++ TUGAS ASISTENSI 1/Storable.java
@@ -1,5 +1,6 @@
 public interface Storable {
     void tambahItem(String item, int jumlah);
+    void tambahItem(String item, int jumlah, String genre);
     void hapusItem(String item);
     void tampilkanItem();
 }
