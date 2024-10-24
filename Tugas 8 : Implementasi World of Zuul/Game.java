075ddb23329d8453f335392e9150709e349b1d4d
crewsakantt <162857687+crewsakantt@users.noreply.github.com>
2024-10-24 10:35:11 +0700

    Create Game.java

---
+++ Tugas 8 : Implementasi World of Zuul/Game.java
@@ -0,0 +1,149 @@
+public class Game  {
+    private Parser parser;
+    private Room currentRoom;
+        
+    public Game() {
+        createRooms();
+        parser = new Parser();
+    }
+
+    private void createRooms() {
+        Room outside, theater, pub, lab, office, library, secretChamber, garden, cafeteria, rooftop;
+    
+        outside = new Room("outside the grand gates of the ancient university, shrouded in mystery and ivy.");
+        theater = new Room("in a grand, old lecture theater with dusty chalkboards and flickering lights.");
+        pub = new Room("in a cozy campus pub with the smell of aged wood and the sound of soft jazz in the background.");
+        lab = new Room("in a futuristic computing lab, full of buzzing servers and flashing screens.");
+        office = new Room("in the head professor's office, cluttered with books and old artifacts.");
+        library = new Room("in a vast, towering library with endless shelves of ancient texts.");
+        secretChamber = new Room("in a hidden chamber beneath the library, filled with strange symbols and glowing crystals.");
+        garden = new Room("in a peaceful garden with blooming flowers and the sound of birds chirping.");
+        cafeteria = new Room("in the bustling campus cafeteria, filled with the aroma of freshly baked bread.");
+        rooftop = new Room("on the rooftop of the university, offering a stunning view of the campus under the stars.");
+    
+        outside.setExits(null, theater, garden, pub);
+        theater.setExits(null, null, null, outside);
+        pub.setExits(null, outside, null, null);
+        garden.setExits(outside, library, cafeteria, null);
+        cafeteria.setExits(garden, null, null, null);
+        lab.setExits(office, rooftop, null, null);
+        office.setExits(null, lab, null, null);
+        library.setExits(null, secretChamber, null, garden);
+        secretChamber.setExits(null, null, library, null);
+        rooftop.setExits(null, null, null, lab);
+    
+        currentRoom = outside;
+    }    
+
+    public void play() {
+        printWelcome();
+                
+        boolean finished = false;
+        while(!finished) {
+            Command command = parser.getCommand();
+            finished = processCommand(command);
+        }
+        System.out.println("Thank you for playing. Good bye.");
+    }
+
+    private void printWelcome() {
+        System.out.println();
+        System.out.println("Welcome to the World of Zuul!");
+        System.out.println("World of Zuul is a new, incredibly boring adventure game.");
+        System.out.println("Type 'help' if you need help.");
+        System.out.println();
+        System.out.println("You are " + currentRoom.getDescription());
+
+        printLocation();
+    }
+
+    private boolean processCommand(Command command) {
+        boolean wantToQuit = false;
+
+        if(command.isUnknown()) {
+            System.out.println("I don't know what you mean...");
+            return false;
+        }
+
+        String commandWord = command.getCommandWord();
+        if(commandWord.equals("help")) {
+            printHelp();
+        }
+        else if(commandWord.equals("go")) {
+            goRoom(command);
+        }
+        else if(commandWord.equals("quit")) {
+            wantToQuit = quit(command);
+        }
+
+        return wantToQuit;
+    }
+
+
+    private void printHelp() {
+        System.out.println("You are lost. You are alone. You wander");
+        System.out.println("around at the university.");
+        System.out.println();
+        System.out.println("Your command words are:");
+        System.out.println("   go quit help");
+    }
+
+    private void goRoom(Command command) {
+        if(!command.hasSecondWord()) {
+            System.out.println("Go where?");
+            return;
+        }
+
+        String direction = command.getSecondWord();
+
+        Room nextRoom = null;
+        if(direction.equals("north")) {
+            nextRoom = currentRoom.northExit;
+        }
+        if(direction.equals("east")) {
+            nextRoom = currentRoom.eastExit;
+        }
+        if(direction.equals("south")) {
+            nextRoom = currentRoom.southExit;
+        }
+        if(direction.equals("west")) {
+            nextRoom = currentRoom.westExit;
+        }
+
+        if(nextRoom == null) {
+            System.out.println("There is no door!");
+        }
+        else {
+            currentRoom = nextRoom;
+            System.out.println("You are " + currentRoom.getDescription());
+            printLocation();
+        }
+    }
+
+    private boolean quit(Command command) {
+        if(command.hasSecondWord()) {
+            System.out.println("Quit what?");
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
+    
+    private void printLocation() {
+        System.out.print("Exits: ");
+        if(currentRoom.northExit != null) {
+            System.out.print("north ");
+        }
+        if(currentRoom.eastExit != null) {
+            System.out.print("east ");
+        }
+        if(currentRoom.southExit != null) {
+            System.out.print("south ");
+        }
+        if(currentRoom.westExit != null) {
+            System.out.print("west ");
+        }
+        System.out.println();
+    }
+}
